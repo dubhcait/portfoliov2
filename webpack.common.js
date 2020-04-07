354330b77e1af599e9812d46abef2e5772c5ca92
Kate O'Brien <kate.obrien@fluidly.com>
2020-04-07 10:29:12 +0100

    Modify webpack config

---
+++ webpack.common.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: "./app/index.js",
+  plugins: [
+    // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Production',
+      template: "app/index.html"
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js"
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: "babel-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] }
+    ]
+  }
+};
